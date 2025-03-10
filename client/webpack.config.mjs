89885f2293d65bf423658f625c7306e095a6055d
Sfacius-DAWI <Sfacius-DAWI>
2025-03-10 14:50:46 +0100

    con docker

---
+++ client/webpack.config.mjs
@@ -0,0 +1,33 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: path.resolve(__dirname, 'src/index.js'), // se ajusta la ruta absoluta
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(process.cwd(), '../server/dist'), // salida para el servidor
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'buble-loader',
+                options: {
+                    objectAssign: 'Object.assign',
+                    transforms: {
+                        modules: false
+                    }
+                },
+            },
+        ],
+    },
+    mode: 'development',
+};
--- server/server.js
+++ server/server.js
@@ -3,33 +3,25 @@ import { fileURLToPath } from 'url';
 import fastify from 'fastify';
 import fastifyStatic from '@fastify/static';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const app = fastify({ logger: true });
 
-
 app.register(fastifyStatic, {
-  root: path.join(__dirname, 'dist'),
+  // Actualiza la ruta para que apunte a c:\Users\santi\Documents\actividad10_js\client\dist
+  root: path.join(__dirname, '..', 'client', 'dist'),
   prefix: '/',
 });
-
-
 
 app.get('/', async (req, reply) => {
   return reply.sendFile('index.html');
 });
 
-
-
-8
-
 const start = async () => {
   try {
     await app.listen({ port: 3001, host: '0.0.0.0' });
-    app.log.info('Servidor corriendo en http://localhost:3000');
+    app.log.info('Servidor corriendo en http://localhost:3001');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
